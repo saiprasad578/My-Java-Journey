fa4e8e65a38433c0cf90394b9e046fe2c0363084
saiprasad578 <saiprasad78v@gmail.com>
2025-10-01 18:40:34 +0530

    day 1 program

---
+++ Basic to advance pro/Swap.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Enter the first number:");
+        int num1 = sc.nextInt(); 
+        System.out.println("Enter the second number:");
+        int num2 = sc.nextInt();
+        System.out.println("Before swapping: num1 = " + num1 + ", num2 = " + num2);
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        System.out.println("After swapping: num1 = " + num1 + ", num2 = " + num2);
+
+        sc.close();
+    }
+}
+
