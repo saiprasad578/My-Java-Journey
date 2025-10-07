579a9d4a2898354eec867b9b3c4402158f81e001
saiprasad578 <saiprasad78v@gmail.com>
2025-10-07 12:10:20 +0530

    java 2 nd day programs

---
+++ Basic to advance pro/multi.java
@@ -0,0 +1,8 @@
+public class multi {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a * b;
+        System.out.println("The multiplication of " + a + " and " + b + " is: " + c);
+    }
+}
