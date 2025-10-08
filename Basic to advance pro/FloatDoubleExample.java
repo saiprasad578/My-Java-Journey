8d75e1578a97f169dd842228b3a270512795132c
saiprasad578 <saiprasad78v@gmail.com>
2025-10-08 19:30:14 +0530

    datatypes

---
+++ Basic to advance pro/FloatDoubleExample.java
@@ -0,0 +1,8 @@
+public class FloatDoubleExample{
+    public static void main(String[] args) {
+        float f = 12.5f;
+        double d = 25.75;
+        System.out.println("Float value: " + f);
+        System.out.println("Double value: " + d);
+    }
+}
