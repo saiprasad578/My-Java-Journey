61a2b6dd07faaaabe871f3acdc0149e366c0fd2e
saiprasad578 <saiprasad78v@gmail.com>
2025-10-09 19:38:08 +0530

    operators pro

---
+++ Basic to advance pro/op.java
@@ -0,0 +1,15 @@
+public class op {
+    public static void main(String[] args) {
+        int a = 4, b = 2;
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+    }
+}
