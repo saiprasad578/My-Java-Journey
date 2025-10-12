cf6d2e931ab9fe0b0f5a99c65cedb46c269cd50e
saiprasad578 <saiprasad78v@gmail.com>
2025-10-12 15:10:47 +0530

    looping statements

---
+++ Basic to advance pro/loop3.java
@@ -0,0 +1,11 @@
+public class loop3 {
+    public static void main(String[] args) {
+        int num = 12345, reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        System.out.println(reversed);
+    }
+}
