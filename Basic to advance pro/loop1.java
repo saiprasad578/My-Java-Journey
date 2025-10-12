ad3f976fd7982e5cab0ab84b049f84abc66c83a1
saiprasad578 <saiprasad78v@gmail.com>
2025-10-12 15:09:33 +0530

    looping statements

---
+++ Basic to advance pro/loop1.java
@@ -0,0 +1,11 @@
+public class loop1 {
+    public static void main(String[] args) {
+        int n = 15, a = 0, b = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
