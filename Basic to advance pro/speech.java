cf744af54e1ab11e6d458a2fbc60c765ce245f8f
saiprasad578 <saiprasad78v@gmail.com>
2025-10-15 19:07:20 +0530

    basic programs

---
+++ Basic to advance pro/speech.java
@@ -0,0 +1,8 @@
+public class speech {
+    public static void main(String[] args) {
+        System.out.println("Hello, everyone!");
+        System.out.println("Welcome to my Java program.");
+        System.out.println("I hope you find it informative and enjoyable.");
+        System.out.println("Thank you for your attention!");
+    }
+}
