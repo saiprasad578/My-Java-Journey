96a6baf098d51563d0407f8b0db7f020e4b04c54
saiprasad578 <saiprasad78v@gmail.com>
2025-10-15 19:07:00 +0530

    basic programs

---
+++ Basic to advance pro/next.java
@@ -0,0 +1,8 @@
+public class next {
+    public static void main(String[] args) {
+        System.out.println("This is the next Java program.");
+        System.out.println("It demonstrates a simple print statement.");
+        System.out.println("Java is fun and exciting!");
+        System.out.println("Let's keep learning and coding!");
+    }
+}
