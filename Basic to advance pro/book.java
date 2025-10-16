7610bbc8c64cd8db92e5ce050cf8908e3310bf17
saiprasad578 <saiprasad78v@gmail.com>
2025-10-16 19:52:43 +0530

    book details

---
+++ Basic to advance pro/book.java
@@ -0,0 +1,13 @@
+public class book{
+    public static void main(String[] args) {
+        String title = "The Great Gatsby";
+        String author = "F. Scott Fitzgerald";
+        int publicationYear = 1925;
+        String isbn = "9780743273565";
+
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Publication Year: " + publicationYear);
+        System.out.println("ISBN: " + isbn);
+    }
+}
