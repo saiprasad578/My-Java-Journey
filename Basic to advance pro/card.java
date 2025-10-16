aae365da5a4cae0d1267fe17a7304e72b38c3857
saiprasad578 <saiprasad78v@gmail.com>
2025-10-16 19:41:20 +0530

    number system

---
+++ Basic to advance pro/card.java
@@ -0,0 +1,13 @@
+public class card{
+    public static void main(String[] args) {
+        int cardNumber = 123456789;
+        String cardHolderName = "John Doe";
+        String expirationDate = "12/25";
+        String cvv = "123";
+
+        System.out.println("Card Number: " + cardNumber);
+        System.out.println("Card Holder Name: " + cardHolderName);
+        System.out.println("Expiration Date: " + expirationDate);
+        System.out.println("CVV: " + cvv);
+    }
+}
---
+++ Basic to advance pro/num.java
@@ -0,0 +1,12 @@
+public class num{
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
