1fd588fb432441ab11060bca67703e38e3a07390
saiprasad578 <saiprasad78v@gmail.com>
2025-10-16 19:42:15 +0530

    vote system

---
+++ Basic to advance pro/vote.java
@@ -0,0 +1,10 @@
+public class vote {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+    }
+}
