68ee2947f901c2fd5f8ab714fd74915333f61793
saiprasad578 <saiprasad78v@gmail.com>
2025-10-17 19:29:39 +0530

    java basic progams

---
+++ Basic to advance pro/git.java
@@ -0,0 +1,13 @@
+public class git {
+    public static void main(String[] args) {
+        String repositoryName = "MyJavaProject";
+        String branchName = "main";
+        String commitHash = "a1b2c3d4";
+        String authorName = "Jane Doe";
+
+        System.out.println("Repository Name: " + repositoryName);
+        System.out.println("Branch Name: " + branchName);
+        System.out.println("Commit Hash: " + commitHash);
+        System.out.println("Author Name: " + authorName);
+    }
+}
