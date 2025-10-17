1b710975ee6c1260a9817417f2e10aa7aa033fe8
saiprasad578 <saiprasad78v@gmail.com>
2025-10-17 19:30:05 +0530

    java basic progams

---
+++ Basic to advance pro/marks.java
@@ -0,0 +1,15 @@
+public class marks {
+    public static void main(String[] args) {
+        String studentName = "Alice Smith";
+        String studentID = "S123456";
+        int mathMarks = 85;
+        int scienceMarks = 90;
+        int englishMarks = 78;
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Math Marks: " + mathMarks);
+        System.out.println("Science Marks: " + scienceMarks);
+        System.out.println("English Marks: " + englishMarks);
+    }
+}
