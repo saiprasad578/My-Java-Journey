2cb3766281b2f9b29cfcac15bc5c7434e94f6924
saiprasad578 <saiprasad78v@gmail.com>
2025-10-17 19:28:21 +0530

    java basic progams

---
+++ Basic to advance pro/atm.java
@@ -0,0 +1,11 @@
+public class atm {
+    public static void main(String[] args) {
+        String accountHolderName = "John Doe";
+        String accountNumber = "123456789";
+        double balance = 1500.75;
+
+        System.out.println("Account Holder Name: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: $" + balance);
+    }
+}
