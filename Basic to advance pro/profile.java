6d89b710bb79d1cfdc2dd7e3bd9adf7e1a1ea9c3
saiprasad578 <saiprasad78v@gmail.com>
2025-10-17 19:29:01 +0530

    java basic progams

---
+++ Basic to advance pro/profile.java
@@ -0,0 +1,11 @@
+public class profile {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int age = 30;
+        String email = "john.doe@example.com";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+    }
+}       
