32c6642fac9e652f43abad43664ece152a6de830
saiprasad578 <saiprasad78v@gmail.com>
2025-10-19 17:28:10 +0530

    array programs

---
+++ Basic to advance pro/ArrayPalindrome.java
@@ -0,0 +1,19 @@
+public class ArrayPalindrome {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 1};
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            if (arr[i] != arr[arr.length - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Array is a palindrome");
+        } else {
+            System.out.println("Array is not a palindrome");
+        }
+    }
+}
