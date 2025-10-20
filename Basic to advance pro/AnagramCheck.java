8615a1431d9e96d15e987f6147485bfd0d29e710
saiprasad578 <saiprasad78v@gmail.com>
2025-10-20 11:18:36 +0530

    strings program

---
+++ Basic to advance pro/AnagramCheck.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (isAnagram(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are NOT anagrams.");
+        }
+    }
+
+    static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+}
