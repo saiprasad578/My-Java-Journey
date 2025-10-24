765b117f77b312323e6bbf1f2b30f413eb5b869f
saiprasad578 <saiprasad78v@gmail.com>
2025-10-24 19:41:48 +0530

    dsa java

---
+++ Basic to advance pro/PalindromeString.java
@@ -0,0 +1,11 @@
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = new StringBuilder(str).reverse().toString();
+        
+        if (str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
