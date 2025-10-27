bfaaebe6c42807c130d939486a088ae371d1dafb
saiprasad578 <saiprasad78v@gmail.com>
2025-10-27 18:31:11 +0530

    solution problems

---
+++ Basic to advance pro/answer.java
@@ -0,0 +1,16 @@
+class answer {
+    public int numberOfBeams(String[] bank) {
+        int prev = 0, ans = 0;
+        for (String row : bank) {
+            int devices = 0;
+            for (char c : row.toCharArray()) {
+                if (c == '1') devices++;
+            }
+            if (devices > 0) {
+                ans += prev * devices;
+                prev = devices;
+            }
+        }
+        return ans;
+    }
+}
