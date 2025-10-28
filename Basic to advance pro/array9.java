197099e87f073cbe422b06fdad2fc0a97ea3cefe
saiprasad578 <saiprasad78v@gmail.com>
2025-10-28 18:49:55 +0530

    array program

---
+++ Basic to advance pro/array9.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+class array9 {
+    boolean simulate(int[] nums, int curr, int dir) {
+        int n = nums.length;
+        int[] a = Arrays.copyOf(nums, n);
+        while (curr >= 0 && curr < n) {
+            if (a[curr] == 0) curr += dir;
+            else {
+                a[curr]--;
+                dir = -dir;
+                curr += dir;
+            }
+        }
+        for (int x : a) if (x != 0) return false;
+        return true;
+    }
+
+    public int countValidSelections(int[] nums) {
+        int n = nums.length, ans = 0;
+        for (int i = 0; i < n; i++)
+            if (nums[i] == 0) {
+                if (simulate(nums, i, 1)) ans++;
+                if (simulate(nums, i, -1)) ans++;
