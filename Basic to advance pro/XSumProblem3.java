fd79939d1d03a50f37e5dc15fbc32b7eababbd55
saiprasad578 <saiprasad78v@gmail.com>
2025-11-04 19:36:50 +0530

    x sum problems

---
+++ Basic to advance pro/XSumProblem3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class XSumProblem3 {
+    public int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        for (int i = 0; i <= n - k; i++) {
+            Map<Integer, Integer> freq = new HashMap<>();
+            for (int j = i; j < i + k; j++) freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
+            List<int[]> list = new ArrayList<>();
+            for (var e : freq.entrySet()) list.add(new int[]{e.getKey(), e.getValue()});
+            list.sort((a, b) -> a[1] == b[1] ? b[0] - a[0] : b[1] - a[1]);
+            Set<Integer> keep = new HashSet<>();
+            for (int t = 0; t < Math.min(x, list.size()); t++) keep.add(list.get(t)[0]);
+            int sum = 0;
+            for (int j = i; j < i + k; j++) if (keep.contains(nums[j])) sum += nums[j];
+            res[i] = sum;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        XSumProblem3 s = new XSumProblem3();
+        int[] nums = {9, 8, 7, 9, 6, 9, 8, 8, 7};
+        int k = 4, x = 3;
+        System.out.println(Arrays.toString(s.findXSum(nums, k, x)));
+        // Expected: [33, 24, 31, 32, 31, 32]
+    }
+}
