8315d94c3283776fe4acf2f552da5bd9badcbfd9
saiprasad578 <saiprasad78v@gmail.com>
2025-11-18 16:07:43 +0530

    1-bit and 2-bit problems

---
+++ Basic to advance pro/1-bit.java
@@ -0,0 +1,9 @@
+class 1-bit {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] == 1 ? 2 : 1;
+        }
+        return i == bits.length - 1;
+    }
+}
