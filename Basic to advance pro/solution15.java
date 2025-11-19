e028092d9b1ef62f9585fa04664fcee07370358c
saiprasad578 <saiprasad78v@gmail.com>
2025-11-19 18:41:50 +0530

    1 and 2 bit problems

---
+++ Basic to advance pro/solution15.java
@@ -0,0 +1,9 @@
+class solution15 {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0;
+        while (i < bits.length - 1) {
+            i += bits[i] == 1 ? 2 : 1;
+        }
+        return i == bits.length - 1;
+    }
+}
