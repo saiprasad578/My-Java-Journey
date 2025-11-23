9de338115ed6cd9672c8f0a7c9c44a46361f04cf
saiprasad578 <saiprasad78v@gmail.com>
2025-11-23 18:23:00 +0530

    greatest sum problem

---
+++ Basic to advance pro/greatest2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class greatest2 {
+    public int maxSumDivThree(int[] nums) {
+        int sum = 0;
+        List<Integer> r1 = new ArrayList<>();
+        List<Integer> r2 = new ArrayList<>();
+        for (int n : nums) {
+            sum += n;
+            if (n % 3 == 1) r1.add(n);
+            else if (n % 3 == 2) r2.add(n);
+        }
+        Collections.sort(r1);
+        Collections.sort(r2);
+        if (sum % 3 == 1) {
+            int a = r1.size() > 0 ? r1.get(0) : Integer.MAX_VALUE;
+            int b = r2.size() > 1 ? r2.get(0) + r2.get(1) : Integer.MAX_VALUE;
+            sum -= Math.min(a, b);
+        } else if (sum % 3 == 2) {
+            int a = r2.size() > 0 ? r2.get(0) : Integer.MAX_VALUE;
+            int b = r1.size() > 1 ? r1.get(0) + r1.get(1) : Integer.MAX_VALUE;
+            sum -= Math.min(a, b);
+        }
+        return sum;
+    }
+}
