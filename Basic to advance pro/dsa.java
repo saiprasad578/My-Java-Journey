4bdd26c1a62f48cbcf2df5a1d6a466e58856a52b
saiprasad578 <saiprasad78v@gmail.com>
2025-12-01 15:53:18 +0530

    dsa problem

---
+++ Basic to advance pro/dsa.java
@@ -0,0 +1,12 @@
+class dsa {
+    public boolean isPalindrome(int x) {
+        String s = Integer.toString(x);
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
