a4c0604704395004811eb8882a02b52e2632fbac
saiprasad578 <saiprasad78v@gmail.com>
2025-12-06 20:22:02 +0530

    stack problem

---
+++ Basic to advance pro/stack.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class stack {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println("Elements: " + list);
+    }
+}
