6757da1db6911462482ee0b5715e580b3282b718
saiprasad578 <saiprasad78v@gmail.com>
2025-12-07 18:29:57 +0530

    graph problem

---
+++ Basic to advance pro/graph7.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class graph7 {
+    private Map<Integer, List<Integer>> adj = new HashMap<>();
+
+    public void addEdge(int u, int v) {
+        adj.computeIfAbsent(u, k -> new ArrayList<>()).add(v);
+        adj.computeIfAbsent(v, k -> new ArrayList<>()).add(u);
+    }
+
+    public void printGraph() {
+        for (int v : adj.keySet()) {
+            System.out.println(v + " -> " + adj.get(v));
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addEdge(1, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+        g.addEdge(3, 4);
+        g.printGraph();
+    }
+}
