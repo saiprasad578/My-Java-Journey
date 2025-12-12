2ce85ea37a5e9be5ee51e2da859d2962a8d87266
saiprasad578 <saiprasad78v@gmail.com>
2025-12-12 20:21:43 +0530

    Polymorphism problem

---
+++ Basic to advance pro/poly.java
@@ -0,0 +1,20 @@
+class poly {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(5, 3));
+        System.out.println(c.add(4.2, 3.8));
+        System.out.println(c.add(1, 2, 3));
+    }
+}
