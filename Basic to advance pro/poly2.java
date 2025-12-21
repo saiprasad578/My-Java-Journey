5bbaa81ee54c099e1946cecb87ea88d6ee389761
saiprasad578 <saiprasad78v@gmail.com>
2025-12-21 19:32:13 +0530

    java problem

--- Basic to advance pro/poly2.java
+++ Basic to advance pro/poly2.java
@@ -1,20 +1,20 @@
-interface poly2 {
+interface Vehicle {
     void start();
 }
 
-class Car implements poly2 {
+class Car implements Vehicle {
     public void start() {
         System.out.println("Car starts");
     }
 }
 
-class Bike implements poly2 {
+class Bike implements Vehicle {
     public void start() {
         System.out.println("Bike starts");
     }
 }
 
-public class Demo {
+public class poly2 {
     public static void main(String[] args) {
         Vehicle v;
 
