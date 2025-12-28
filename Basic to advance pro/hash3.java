b04f7de839b0bb207e3bb0bff15b432172fe9460
saiprasad578 <saiprasad78v@gmail.com>
2025-12-28 13:21:15 +0530

    hash function

---
+++ Basic to advance pro/hash3.java
@@ -0,0 +1,10 @@
+public class hash3 {
+    public static void main(String[] args) {
+        String username = "Sai";
+
+        int hash = username.length();
+
+        System.out.println("Username: " + username);
+        System.out.println("Hash Value: " + hash);
+    }
+}
