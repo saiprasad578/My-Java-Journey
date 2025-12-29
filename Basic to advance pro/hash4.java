cad6856b70b2f849fb65d23e91117722e7f0b443
saiprasad578 <saiprasad78v@gmail.com>
2025-12-29 19:22:54 +0530

    hash function

---
+++ Basic to advance pro/hash4.java
@@ -0,0 +1,10 @@
+public class hash4 {
+    public static void main(String[] args) {
+        int key = 12;
+
+        int hash = key * key;
+
+        System.out.println("Key: " + key);
+        System.out.println("Hash Value: " + hash);
+    }
+}
