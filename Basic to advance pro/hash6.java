8ea7056f902fe424fdaff6b88d8aa61125f6d165
saiprasad578 <saiprasad78v@gmail.com>
2025-12-30 19:09:53 +0530

    hash function

--- Basic to advance pro/hash6.java
+++ Basic to advance pro/hash6.java
@@ -1,10 +1,39 @@
 public class hash6 {
-    public static void main(String[] args) {
-        int a = 5;
-        int b = 3;
-        int sum = a + b;
 
-        System.out.println("Hello, Java!");
-        System.out.println("Sum = " + sum);
+    static int[] table = new int[10];
+
+    // Initialize table with -1
+    static void initialize() {
+        for (int i = 0; i < table.length; i++) {
+            table[i] = -1;
+        }
+    }
+
+    static void insert(int key) {
+        int index = key % table.length;
+
+        // Linear probing
+        while (table[index] != -1) {
+            index = (index + 1) % table.length;
+        }
+        table[index] = key;
+    }
+
+    static void display() {
+        System.out.println("Hash Table:");
+        for (int i = 0; i < table.length; i++) {
+            System.out.println(i + " : " + table[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        initialize();
+
+        insert(12);
+        insert(22);
+        insert(32);
+        insert(42);
+
+        display();
     }
 }
