5845ae46ad314560661469c6dbe1e6f1bc42e4ee
saiprasad578 <saiprasad78v@gmail.com>
2026-01-05 19:05:17 +0530

    logic program

---
+++ Basic to advance pro/PrimeCheck.java
@@ -0,0 +1,17 @@
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+
+        if (num <= 1) isPrime = false;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        System.out.println(isPrime ? "Prime" : "Not Prime");
+    }
+}
