20997093f3e2bf2f9b6f16ff8c414254653017f3
saiprasad578 <saiprasad78v@gmail.com>
2026-01-07 18:56:05 +0530

    logical problem

--- Basic to advance pro/1-bit2.java
+++
@@ -1,9 +0,0 @@
-class 1-bit2 {
-    public boolean isOneBitCharacter(int[] bits) {
-        int count = 0;
-        for (int i = bits.length - 2; i >= 0 && bits[i] == 1; i--) {
-            count++;
-        }
-        return count % 2 == 0;
-    }
-}
