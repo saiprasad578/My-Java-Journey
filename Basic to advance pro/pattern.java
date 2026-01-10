2379e6dd75214772f8cabe785489b457791f2ee3
saiprasad578 <saiprasad78v@gmail.com>
2026-01-10 19:35:58 +0530

    all partens

---
+++ Basic to advance pro/pattern.java
@@ -0,0 +1,123 @@
+public class pattern {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        // 1. Square Pattern
+        System.out.println("1. Square Pattern");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 2. Right Triangle
+        System.out.println("\n2. Right Triangle");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 3. Inverted Right Triangle
+        System.out.println("\n3. Inverted Right Triangle");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 4. Pyramid
+        System.out.println("\n4. Pyramid");
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 5. Inverted Pyramid
+        System.out.println("\n5. Inverted Pyramid");
+        for (int i = n; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 6. Diamond Pattern
+        System.out.println("\n6. Diamond Pattern");
+
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 7. Number Triangle
+        System.out.println("\n7. Number Triangle");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        // 8. Same Number Pattern
+        System.out.println("\n8. Same Number Pattern");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        // 9. Floyd’s Triangle
+        System.out.println("\n9. Floyd's Triangle");
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num++ + " ");
+            }
+            System.out.println();
+        }
+
+        // 10. Hollow Square
+        System.out.println("\n10. Hollow Square");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
