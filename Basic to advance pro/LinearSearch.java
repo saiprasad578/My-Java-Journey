1757ac6e95cb47610777b3ce5dbe3022ab5e91cb
saiprasad578 <saiprasad78v@gmail.com>
2026-01-12 13:35:39 +0530

    LinearSearch problem

---
+++ Basic to advance pro/LinearSearch.java
@@ -0,0 +1,14 @@
+class LinearSearch {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40};
+        int key = 30;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == key) {
+                System.out.println("Found at index " + i);
+                return;
+            }
+        }
+        System.out.println("Not Found");
+    }
+}
