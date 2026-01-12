1132c7c2a31ae3c66c13575c2de780f3a10a39f1
saiprasad578 <saiprasad78v@gmail.com>
2026-01-12 13:12:58 +0530

    changes

--- Basic to advance pro/tree.java
+++ Basic to advance pro/tree.java
@@ -1,8 +1,25 @@
-class tree {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+    }
+}
+
+class Tree {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         Stack<TreeNode> st = new Stack<>();
         TreeNode cur = root;
+
         while (cur != null || !st.isEmpty()) {
             while (cur != null) {
                 st.push(cur);
