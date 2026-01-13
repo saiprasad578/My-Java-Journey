ad9296a5ca4ed5f0390731187482f1013af12bfc
saiprasad578 <saiprasad78v@gmail.com>
2026-01-13 16:05:03 +0530

    changes

--- Basic to advance pro/array9.java
+++ Basic to advance pro/array9.java
@@ -1,23 +1,43 @@
 import java.util.Arrays;
+
 class array9 {
+
     boolean simulate(int[] nums, int curr, int dir) {
         int n = nums.length;
         int[] a = Arrays.copyOf(nums, n);
+
         while (curr >= 0 && curr < n) {
-            if (a[curr] == 0) curr += dir;
-            else {
+            if (a[curr] == 0) {
+                curr += dir;
+            } else {
                 a[curr]--;
                 dir = -dir;
                 curr += dir;
             }
         }
-        for (int x : a) if (x != 0) return false;
+
+        for (int x : a)
+            if (x != 0)
+                return false;
+
         return true;
     }
 
     public int countValidSelections(int[] nums) {
         int n = nums.length, ans = 0;
-        for (int i = 0; i < n; i++)
+
+        for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 if (simulate(nums, i, 1)) ans++;
                 if (simulate(nums, i, -1)) ans++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        array9 obj = new array9();
+        int[] nums = {0, 1, 0, 1};
+        System.out.println(obj.countValidSelections(nums));
+    }
+}
