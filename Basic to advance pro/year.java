3d9320b2dad4eab3b43b0686074c2121a4d289f4
saiprasad578 <saiprasad78v@gmail.com>
2026-01-25 13:34:46 +0530

    year program

---
+++ Basic to advance pro/year.java
@@ -0,0 +1,8 @@
+public class year {
+    public static void main(String[] args) {
+        int year = 2026;
+        int yearCode = year % 100;
+
+        System.out.println("Year code: " + yearCode);
+    }
+}
