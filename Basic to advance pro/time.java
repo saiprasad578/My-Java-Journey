3f2d95531b8b7cdedc629dc8a4c80ee467480872
saiprasad578 <saiprasad78v@gmail.com>
2026-02-09 19:22:39 +0530

    problem

--- Basic to advance pro/time.java
+++ Basic to advance pro/time.java
@@ -1,14 +1,25 @@
 import java.util.*;
-class time {
+
+class Time {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         int[][] grid = new int[m][n];
-        for (int[] g : guards) grid[g[0]][g[1]] = 1;
-        for (int[] w : walls) grid[w[0]][w[1]] = 2;
+
+        for (int[] g : guards) {
+            grid[g[0]][g[1]] = 1; // guard
+        }
+
+        for (int[] w : walls) {
+            grid[w[0]][w[1]] = 2; // wall
+        }
+
         boolean[][] seen = new boolean[m][n];
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
         for (int[] g : guards) {
             for (int[] d : dirs) {
-                int r = g[0] + d[0], c = g[1] + d[1];
+                int r = g[0] + d[0];
+                int c = g[1] + d[1];
+
                 while (r >= 0 && c >= 0 && r < m && c < n && grid[r][c] == 0) {
                     seen[r][c] = true;
                     r += d[0];
@@ -16,10 +27,15 @@ class time {
                 }
             }
         }
+
         int res = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (grid[i][j] == 0 && !seen[i][j]) res++;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0 && !seen[i][j]) {
+                    res++;
+                }
+            }
+        }
         return res;
     }
 }
