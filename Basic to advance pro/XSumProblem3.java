ec451adcdab291b7fee1c33e0773afe5175a32be
saiprasad578 <saiprasad78v@gmail.com>
2026-02-10 20:21:58 +0530

    modify

--- Basic to advance pro/XSumProblem3.java
+++ Basic to advance pro/XSumProblem3.java
@@ -4,36 +4,42 @@ public class XSumProblem3 {
 
     public int[] findXSum(int[] nums, int k, int x) {
         int n = nums.length;
+        if (k > n) return new int[0];
+
         int[] res = new int[n - k + 1];
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        // first window
+        for (int i = 0; i < k; i++)
+            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
 
         for (int i = 0; i <= n - k; i++) {
-            Map<Integer, Integer> freq = new HashMap<>();
-
-            for (int j = i; j < i + k; j++) {
-                freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
-            }
 
             List<int[]> list = new ArrayList<>();
-            for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
+            for (var e : freq.entrySet())
                 list.add(new int[]{e.getKey(), e.getValue()});
-            }
 
-            list.sort((a, b) -> {
-                if (a[1] != b[1]) return Integer.compare(b[1], a[1]);
-                return Integer.compare(b[0], a[0]);
-            });
+            list.sort((a, b) ->
+                a[1] != b[1] ? b[1] - a[1] : b[0] - a[0]
+            );
 
             Set<Integer> keep = new HashSet<>();
-            for (int t = 0; t < Math.min(x, list.size()); t++) {
+            for (int t = 0; t < Math.min(x, list.size()); t++)
                 keep.add(list.get(t)[0]);
-            }
 
             int sum = 0;
-            for (int j = i; j < i + k; j++) {
+            for (int j = i; j < i + k; j++)
                 if (keep.contains(nums[j])) sum += nums[j];
-            }
 
             res[i] = sum;
+
+            // slide window
+            if (i + k < n) {
+                freq.put(nums[i], freq.get(nums[i]) - 1);
+                if (freq.get(nums[i]) == 0) freq.remove(nums[i]);
+
+                freq.put(nums[i + k], freq.getOrDefault(nums[i + k], 0) + 1);
+            }
         }
         return res;
     }
