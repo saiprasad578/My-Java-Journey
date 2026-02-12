e62c0219726a2e7d1c560b28468dba8e27b0bae7
saiprasad578 <saiprasad78v@gmail.com>
2026-02-12 20:11:17 +0530

    changes

--- Basic to advance pro/song.java
+++ Basic to advance pro/song.java
@@ -1,10 +1,14 @@
 public class song {
     public static void main(String[] args) {
-        System.out.println("Twinkle, twinkle, little star,");
-        System.out.println("How I wonder what you are!");
-        System.out.println("Up above the world so high,");
-        System.out.println("Like a diamond in the sky.");
-        System.out.println("Twinkle, twinkle, little star,");
-        System.out.println("How I wonder what you are!");
+        String lyrics = """
+                Twinkle, twinkle, little star,
+                How I wonder what you are!
+                Up above the world so high,
+                Like a diamond in the sky.
+                Twinkle, twinkle, little star,
+                How I wonder what you are!
+                """;
+
+        System.out.println(lyrics);
     }
 }
