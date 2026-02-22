e1c8af896433b2f129dff51971bcbcb4b4e00a82
saiprasad578 <saiprasad78v@gmail.com>
2026-02-22 08:53:35 +0530

    modify

--- Basic to advance pro/Node2.java
+++ Basic to advance pro/Node2.java
@@ -1,21 +1,25 @@
 class Node2 {
     int data;
     Node2 next;
+
     Node2(int d) {
         data = d;
+        next = null;
     }
 }
 
 public class LinkedList {
-    Node head;
+    Node2 head;
 
     void add(int data) {
-        Node newNode = new Node(data);
+        Node2 newNode = new Node2(data);
+
         if (head == null) {
             head = newNode;
             return;
         }
-        Node temp = head;
+
+        Node2 temp = head;
         while (temp.next != null) {
             temp = temp.next;
         }
@@ -23,7 +27,51 @@ public class LinkedList {
     }
 
     void display() {
-        Node temp = head;
+        Node2 temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.display();
+    }
+}class Node2 {
+    int data;
+    Node2 next;
+
+    Node2(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    Node2 head;
+
+    void add(int data) {
+        Node2 newNode = new Node2(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node2 temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void display() {
+        Node2 temp = head;
         while (temp != null) {
             System.out.print(temp.data + " ");
             temp = temp.next;
