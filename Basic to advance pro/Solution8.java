3f524bfbd1ff92a9032cba69c3bfd2a10f22b310
saiprasad578 <saiprasad78v@gmail.com>
2026-02-25 18:53:27 +0530

    modified changes

--- Basic to advance pro/Solution8.java
+++ Basic to advance pro/Solution8.java
@@ -1,58 +1,17 @@
-import java.util.*;
-
-public class Solution8 {
-
-    public int[] findXSum(int[] nums, int k, int x) {
+class Solution8 {
+    public long maxSubarraySum(int[] nums, int k) {
         int n = nums.length;
-        if (k > n) return new int[0];
-
-        int[] res = new int[n - k + 1];
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        // first window
-        for (int i = 0; i < k; i++)
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
-
-        for (int i = 0; i <= n - k; i++) {
-
-            List<int[]> list = new ArrayList<>();
-
-            for (Map.Entry<Integer, Integer> e : freq.entrySet()) {
-                list.add(new int[]{e.getKey(), e.getValue()});
-            }
-
-            list.sort(new Comparator<int[]>() {
-                public int compare(int[] a, int[] b) {
-                    if (a[1] != b[1]) return b[1] - a[1];
-                    return b[0] - a[0];
-                }
-            });
-
-            Set<Integer> keep = new HashSet<>();
-            for (int t = 0; t < Math.min(x, list.size()); t++)
-                keep.add(list.get(t)[0]);
-
-            int sum = 0;
-            for (int j = i; j < i + k; j++)
-                if (keep.contains(nums[j])) sum += nums[j];
-
-            res[i] = sum;
-
-            // slide window
-            if (i + k < n) {
-                freq.put(nums[i], freq.get(nums[i]) - 1);
-                if (freq.get(nums[i]) == 0) freq.remove(nums[i]);
-
-                freq.put(nums[i + k], freq.getOrDefault(nums[i + k], 0) + 1);
-            }
+        long[] minPref = new long[k];
+        for (int i = 0; i < k; i++) minPref[i] = Long.MAX_VALUE;
+        long prefix = 0;
+        minPref[0] = 0;
+        long ans = Long.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+            int r = (i + 1) % k;
+            if (minPref[r] != Long.MAX_VALUE) ans = Math.max(ans, prefix - minPref[r]);
+            if (prefix < minPref[r]) minPref[r] = prefix;
         }
-        return res;
-    }
-
-    public static void main(String[] args) {
-        Solution8 s = new Solution8();
-        int[] nums = {9, 8, 7, 9, 6, 9, 8, 8, 7};
-        int k = 4, x = 3;
-        System.out.println(Arrays.toString(s.findXSum(nums, k, x)));
+        return ans;
     }
 }
