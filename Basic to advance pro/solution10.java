e8a1d7df0c7c5db57b847296d888cfa239da16f9
saiprasad578 <saiprasad78v@gmail.com>
2026-02-27 18:55:56 +0530

    modified changes

---
+++ Basic to advance pro/solution10.java
@@ -0,0 +1,11 @@
+class solution10 {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> ans = new ArrayList<>();
+        int mod = 0;
+        for (int b : nums) {
+            mod = ((mod << 1) + b) % 5;
+            ans.add(mod == 0);
+        }
+        return ans;
+    }
+}
