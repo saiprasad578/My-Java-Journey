0ff476e3e2beb766a5ec51a0f073fe13e5177e1a
saiprasad578 <saiprasad78v@gmail.com>
2026-03-02 11:23:38 +0530

    modified

--- Basic to advance pro/PalindromeString.java
+++ Basic to advance pro/PalindromeString.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
+
 public class PalindromeString {
-    public static void main(String[] args) {
-        String str = "madam";
-        String rev = new StringBuilder(str).reverse().toString();
+
+    // Method to check palindrome
+    public static boolean isPalindrome(String str) {
         
-        if (str.equals(rev))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
+        // Remove special characters and spaces
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        String rev = new StringBuilder(str).reverse().toString();
+        return str.equals(rev);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a string (or type 'exit' to quit): ");
+            String input = sc.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Program Ended.");
+                break;
+            }
+
+            if (isPalindrome(input))
+                System.out.println("Palindrome ");
+            else
+                System.out.println("Not Palindrome ");
+
+            System.out.println();
+        }
+
+        sc.close();
     }
 }
