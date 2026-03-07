ed23f3fa91fc756358e3c4e63435e59c28acdf47
saiprasad578 <saiprasad78v@gmail.com>
2026-03-07 19:43:48 +0530

    new dsa problem

---
+++ Basic to advance pro/Solution21.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Solution21 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        float b = sc.nextFloat();
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Concatenation: " + a + "" + b);
+        System.out.println("Double Sum: " + ((a + b) * 2));
+    }
+}
