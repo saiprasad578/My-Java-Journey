6d7dd361bb680bc4f6205c0fb6a42e3fb05ce127
saiprasad578 <saiprasad78v@gmail.com>
2026-03-11 04:20:55 +0530

    problems

---
+++ Basic to advance pro/Solution26.java
@@ -0,0 +1,7 @@
+public class Solution26 {
+    public static void main (String args []){
+        int [] nums = {0,1,1};
+        Solution10 s = new Solution10();
+        System.out.println(s.prefixesDivBy5(nums));
+    }
+}
