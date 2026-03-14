0b6a8c94da6cc742be7b1ae5c9b9739c3572732b
saiprasad578 <saiprasad78v@gmail.com>
2026-03-14 16:04:59 +0530

    modified

--- Basic to advance pro/count2.java
+++ Basic to advance pro/count2.java
@@ -4,20 +4,39 @@ public class count2 {
 
     public static void main(String[] args) {
 
-        int[][] points = {
-            {1,2},
-            {3,1},
-            {2,4}
-        };
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter number of points: ");
+        int n = sc.nextInt();
+
+        int[][] points = new int[n][2];
+
+        // Taking input
+        for(int i = 0; i < n; i++) {
+            System.out.print("Enter x and y for point " + (i+1) + ": ");
+            points[i][0] = sc.nextInt();
+            points[i][1] = sc.nextInt();
+        }
+
+        // Print before sorting
+        System.out.println("\nPoints before sorting:");
+        for(int[] p : points) {
+            System.out.println(p[0] + " " + p[1]);
+        }
+
+        // Sorting based on y coordinate
         Arrays.sort(points, new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 return Integer.compare(a[1], b[1]);
             }
         });
 
-        for(int[] p : points){
+        // Print after sorting
+        System.out.println("\nPoints after sorting based on y value:");
+        for(int[] p : points) {
             System.out.println(p[0] + " " + p[1]);
         }
+
+        sc.close();
     }
 }
