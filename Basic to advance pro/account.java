ae237bbe89c168bc2f8bc925c68c348d25523260
saiprasad578 <saiprasad78v@gmail.com>
2026-03-14 16:03:14 +0530

    modified

--- Basic to advance pro/account.java
+++ Basic to advance pro/account.java
@@ -1,17 +1,60 @@
-class account {
+class Account {
     private double balance;
+    private String accountHolder;
 
+    // Setter for account holder
+    void setAccountHolder(String name) {
+        accountHolder = name;
+    }
+
+    // Getter for account holder
+    String getAccountHolder() {
+        return accountHolder;
+    }
+
+    // Setter for balance
     void setBalance(double b) {
         balance = b;
     }
 
+    // Getter for balance
     double getBalance() {
         return balance;
     }
 
+    // Deposit method
+    void deposit(double amount) {
+        balance = balance + amount;
+        System.out.println("Deposited: " + amount);
+    }
+
+    // Withdraw method
+    void withdraw(double amount) {
+        if (amount <= balance) {
+            balance = balance - amount;
+            System.out.println("Withdrawn: " + amount);
+        } else {
+            System.out.println("Insufficient Balance!");
+        }
+    }
+
+    // Display account details
+    void displayDetails() {
+        System.out.println("Account Holder: " + accountHolder);
+        System.out.println("Current Balance: " + balance);
+    }
+
     public static void main(String[] args) {
-        account a = new account();
+        Account a = new Account();
+
+        a.setAccountHolder("Sai Prasad");
         a.setBalance(5000);
-        System.out.println(a.getBalance());
+
+        a.displayDetails();
+
+        a.deposit(2000);
+        a.withdraw(1500);
+
+        System.out.println("Final Balance: " + a.getBalance());
     }
 }
