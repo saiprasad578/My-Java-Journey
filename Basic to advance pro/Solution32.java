10566e160c97291cbdbe8273f7876bcf51785ae9
saiprasad578 <saiprasad78v@gmail.com>
2026-03-15 17:42:03 +0530

    modified

--- Basic to advance pro/Solution32.java
+++ Basic to advance pro/Solution32.java
@@ -1,14 +1,36 @@
+import java.util.*;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
 class Solution32 {
     public ListNode modifiedList(int[] nums, ListNode head) {
         Set<Integer> set = new HashSet<>();
-        for (int n : nums) set.add(n);
+
+        for (int n : nums) {
+            set.add(n);
+        }
+
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+
         ListNode curr = dummy;
+
         while (curr.next != null) {
-            if (set.contains(curr.next.val)) curr.next = curr.next.next;
-            else curr = curr.next;
+            if (set.contains(curr.next.val)) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
         }
+
         return dummy.next;
     }
 }
