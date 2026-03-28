8cbe3428a39b835d332655f2a57a8fbc3555f5ed
saiprasad578 <saiprasad78v@gmail.com>
2026-03-28 19:11:05 +0530

    for loop

---
+++ Basic to advance pro/Solution54.java
@@ -0,0 +1,9 @@
+public class Solution54 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
