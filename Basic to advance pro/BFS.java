a9a360c4add55d63b0015fe4f0e8a358008289ca
saiprasad578 <saiprasad78v@gmail.com>
2026-04-26 17:57:46 +0530

    dsa problems

---
+++ Basic to advance pro/BFS.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class BFS {
+    private LinkedList<Integer> adj[];
+
+    BFS(int v) {
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void bfs(int start) {
+        boolean visited[] = new boolean[adj.length];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int n : adj[node]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BFS g = new BFS(5);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 3);
+        g.addEdge(2, 4);
+
+        System.out.println("BFS Traversal:");
+        g.bfs(0);
+    }
+}
