cd02dabdd608cb975d2bd9f3a1a2ff100018505e
saiprasad578 <saiprasad78v@gmail.com>
2026-04-29 19:15:48 +0530

    dsa problem

---
+++ Basic to advance pro/PalindromeCheck.java
@@ -0,0 +1,24 @@
+public class PalindromeCheck {
+
+    static boolean isPalindrome(String str) {
+        int left = 0, right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+
+        if (isPalindrome(str))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
