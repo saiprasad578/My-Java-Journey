fd15cf1c651165a3128f850e60cc8bb994f2469b
saiprasad578 <saiprasad78v@gmail.com>
2026-04-29 19:20:33 +0530

    dsa problem

---
+++ Basic to advance pro/Solution82.java
@@ -0,0 +1,20 @@
+public class Solution82 {
+
+    static boolean isSorted(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        if (isSorted(arr))
+            System.out.println("Array is Sorted");
+        else
+            System.out.println("Array is NOT Sorted");
+    }
+}
