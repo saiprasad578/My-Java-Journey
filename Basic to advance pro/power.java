173c4ea4fd4b2e074eb694d4f06e119e995dd88f
saiprasad578 <saiprasad78v@gmail.com>
2026-04-29 19:22:44 +0530

    modified

--- Basic to advance pro/power.java
+++ Basic to advance pro/power.java
@@ -1,5 +1,17 @@
 class power {
+
     public boolean isPowerOfTwo(int n) {
         return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        Power obj = new Power();
+
+        int n = 16; // change value here
+
+        if (obj.isPowerOfTwo(n))
+            System.out.println(n + " is a Power of Two");
+        else
+            System.out.println(n + " is NOT a Power of Two");
     }
 }
