a272ebffb8cd49efec51ea1c8f81d84dd4d12499
saiprasad578 <saiprasad78v@gmail.com>
2026-04-29 19:17:07 +0530

    dsa problem

---
+++ Basic to advance pro/ReverseArray.java
@@ -0,0 +1,26 @@
+public class ReverseArray {
+
+    static void reverse(int arr[]) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        reverse(arr);
+
+        System.out.println("Reversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
