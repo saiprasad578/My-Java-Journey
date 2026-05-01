776220cd1a7074b7e51b2fba5f4e4e6c336ab19c
saiprasad578 <saiprasad78v@gmail.com>
2026-05-01 18:43:23 +0530

    dsa problem

---
+++ Basic to advance pro/Solution84.java
@@ -0,0 +1,14 @@
+public class Solution84 {
+
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        int a = 48, b = 18;
+
+        System.out.println("GCD: " + gcd(a, b));
+    }
+}
