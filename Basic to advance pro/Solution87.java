1620073dfafff271e9082c55a13e195fc0d46e66
saiprasad578 <saiprasad78v@gmail.com>
2026-05-03 07:54:57 +0530

    array traversal modified

--- Basic to advance pro/Solution87.java
+++ Basic to advance pro/Solution87.java
@@ -1,10 +1,10 @@
 public class Solution87 {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
+        int a [] = {1, 2, 3, 4, 5};
         int sum = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+        for (int i = 0; i < a.length; i++) {
+            sum += a [i];
         }
 
         System.out.println("Sum = " + sum);
--- Basic to advance pro/Solution88.java
+++ Basic to advance pro/Solution88.java
@@ -1,10 +1,10 @@
 public class Solution88 {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6};
+        int a[] = {1, 2, 3, 4, 5, 6};
 
         int even = 0, odd = 0;
 
-        for (int num : arr) {
+        for (int num : a) {
             if (num % 2 == 0)
                 even++;
             else
