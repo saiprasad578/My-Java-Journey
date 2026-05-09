eae5b52947b40cf8ff684c96ef90aebc87a0d359
saiprasad578 <saiprasad78v@gmail.com>
2026-05-09 18:21:32 +0530

    dsa

---
+++ Basic to advance pro/Solution105.java
@@ -0,0 +1,15 @@
+public class Solution105 {
+
+    public static void main(String[] args) {
+
+        String str = "Hello";
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + rev);
+    }
+}
