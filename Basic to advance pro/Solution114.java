35e36af242d4ecb884e8a2862092e17d5e1b3f7f
saiprasad578 <saiprasad78v@gmail.com>
2026-05-11 20:16:35 +0530

    modified

--- Basic to advance pro/Solution114.java
+++ Basic to advance pro/Solution114.java
@@ -1,4 +1,4 @@
-public class StringExample {
+public class Solution114{
     public static void main(String[] args) {
         String name = "Java Programming";
         System.out.println("Welcome to " + name);
