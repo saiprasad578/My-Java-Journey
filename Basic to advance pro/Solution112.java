a0e8bde74fd89d6a2cbd62186fc3fe76028ed0cc
saiprasad578 <saiprasad78v@gmail.com>
2026-05-11 20:08:08 +0530

    dsa

---
+++ Basic to advance pro/Solution112.java
@@ -0,0 +1,11 @@
+public class Solution112 {
+
+    public static void main(String[] args) {
+
+        String str = "Java is easy to learn";
+
+        String words[] = str.split(" ");
+
+        System.out.println("Number of words: " + words.length);
+    }
+}
