0b39cfbb6567fad8cfa979bf8827af47db6973fb
saiprasad578 <saiprasad78v@gmail.com>
2026-05-19 17:13:10 +0530

    this a basic for loop

---
+++ Basic to advance pro/forloop.java
@@ -0,0 +1,14 @@
+public class forloop {
+public static void main (String args []) {
+    for (int i = 0; i < 5; i++) {
+        System.out.println("Hello, World! " + i);
+        if (i == 2) {
+            System.out.println("i is equal to 2");
+        }
+        else {
+            System.out.println("i is not equal to 2");
+        } 
+        System.out.println("This is the end of iteration " + i);
+    }
+}    
+}
