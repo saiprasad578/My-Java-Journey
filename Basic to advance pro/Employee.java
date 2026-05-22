97df00de4890bb2b4942ff5b6cb33174b87de959
saiprasad578 <saiprasad78v@gmail.com>
2026-05-22 18:03:54 +0530

    java problems

---
+++ Basic to advance pro/Employee.java
@@ -0,0 +1,37 @@
+class Employee {
+
+    // Private variables
+    private String name;
+    private double salary;
+
+    // Setter methods
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setSalary(double s) {
+        salary = s;
+    }
+
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee emp = new Employee();
+
+        emp.setName("Sai");
+        emp.setSalary(50000);
+
+        System.out.println("Employee Name: " + emp.getName());
+        System.out.println("Employee Salary: " + emp.getSalary());
+    }
+}
