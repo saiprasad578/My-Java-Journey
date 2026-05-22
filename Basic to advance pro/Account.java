e46c730f59f04d5c044aec3ad167a17a0d2e4c8c
saiprasad578 <saiprasad78v@gmail.com>
2026-05-22 18:01:38 +0530

    java problems

---
+++ Basic to advance pro/Account.java
@@ -0,0 +1,28 @@
+class Account {
+
+    // Private data member
+    private int pin = 1234;
+
+    // Getter method
+    public int getPin() {
+        return pin;
+    }
+
+    // Setter method
+    public void setPin(int newPin) {
+        pin = newPin;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account a = new Account();
+
+        System.out.println("Old PIN: " + a.getPin());
+
+        a.setPin(5678);
+
+        System.out.println("New PIN: " + a.getPin());
+    }
+}
