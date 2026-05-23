5817c2b5e211fe28e117379bd6c64f486b632015
saiprasad578 <saiprasad78v@gmail.com>
2026-05-23 19:34:15 +0530

    oops concepts

---
+++ Basic to advance pro/car.java
@@ -0,0 +1,37 @@
+class Car {
+
+    // Private variables
+    private String brand;
+    private int speed;
+
+    // Setter methods
+    public void setBrand(String b) {
+        brand = b;
+    }
+
+    public void setSpeed(int s) {
+        speed = s;
+    }
+
+    // Getter methods
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car c = new Car();
+
+        c.setBrand("BMW");
+        c.setSpeed(180);
+
+        System.out.println("Car Brand: " + c.getBrand());
+        System.out.println("Car Speed: " + c.getSpeed());
+    }
+}
