2ebe7352f0101b608fb97d0be4295615ce86e3cc
saiprasad578 <saiprasad78v@gmail.com>
2026-05-30 19:08:48 +0530

    2d array problem

--- Basic to advance pro/PalindromeString2.java
+++ Basic to advance pro/PalindromeString2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeString {
+public class PalindromeString2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
