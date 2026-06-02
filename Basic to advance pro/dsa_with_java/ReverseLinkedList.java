31704bb28f5b3d5f0e5b36611608191a6e775514
saiprasad578 <saiprasad78v@gmail.com>
2026-06-02 18:57:35 +0530

    linked list problem

---
+++ Basic to advance pro/dsa_with_java/ReverseLinkedList.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data) {
+        this.data = data;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverse(head);
+
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
