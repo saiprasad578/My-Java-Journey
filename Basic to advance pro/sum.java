a2b2c507cb8a537fd8b35d2e463c537bf2b6f232
saiprasad578 <saiprasad78v@gmail.com>
2026-06-15 07:43:20 +0530

    modified problem

--- Basic to advance pro/sum.java
+++ Basic to advance pro/sum.java
@@ -1,8 +1,19 @@
-public class sum{
-    public static void main(String args[]){
-        int a=1;
-        int b=9;
-        int sum=a+b;
-        System.out.println("sum="+sum);
-    } 
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+
+        sc.close();
+    }
 }
