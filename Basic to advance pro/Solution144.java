c1afce437522489cc2c485c0f09cbb577705e638
saiprasad578 <saiprasad78v@gmail.com>
2026-06-20 18:55:20 +0530

    java problems

---
+++ Basic to advance pro/Solution144.java
@@ -0,0 +1,11 @@
+public class Solution144 {
+    
+    public static int toggleBit(int number, int position) {
+       
+        return 0; 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toggleBit(5, 1)); 
+    }
+}
