09ba6a81e0f79df350b62be219db813871ea4d62
saiprasad578 <saiprasad78v@gmail.com>
2026-07-01 20:08:24 +0530

    todays problem of the day

---
+++ Basic to advance pro/dsa_with_java/SecondLargest.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            System.out.println("No second largest element.");
+        } else {
+            System.out.println("Second Largest Element: " + secondLargest);
+        }
+
+        sc.close();
+    }
+}
