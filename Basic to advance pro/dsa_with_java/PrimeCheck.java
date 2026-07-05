86af03309fdbf5ff424f4beba4f020fd13fdfeb2
saiprasad578 <saiprasad78v@gmail.com>
2026-07-05 19:54:09 +0530

    problem of the day

--- Basic to advance pro/dsa_with_java/PrimeCheck.java
+++ Basic to advance pro/dsa_with_java/PrimeCheck.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class PrimeCheck {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.class);
+        // FIXED: Changed System.class to System.in
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number to check: ");
         int number = scanner.nextInt();
         
         boolean isPrime = true;
         
-        // 0 and 1 are not prime numbers
+       
         if (number <= 1) {
             isPrime = false;
         } else {
-            // Check for factors up to the square root of the number
+            
             for (int i = 2; i <= Math.sqrt(number); i++) {
                 if (number % i == 0) {
                     isPrime = false; // Found a factor, so it's not prime
@@ -21,7 +22,7 @@ public class PrimeCheck {
             }
         }
         
-        // Output the result
+     
         if (isPrime) {
             System.out.println(number + " is a prime number.");
         } else {
