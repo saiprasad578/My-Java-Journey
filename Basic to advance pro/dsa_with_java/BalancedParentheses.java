657010fe61821b5e42b06ceb5c7a4107c7f4c18c
saiprasad578 <saiprasad78v@gmail.com>
2026-07-18 19:10:37 +0530

    problem of the day

---
+++ Basic to advance pro/dsa_with_java/BalancedParentheses.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class BalancedParentheses {
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String input = "{[()]}";
+        System.out.println("Balanced: " + isBalanced(input));
+    }
+}
