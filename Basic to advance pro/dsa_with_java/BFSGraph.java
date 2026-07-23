e2a23de381718620a053f219e9f8225d55ef0ca2
saiprasad578 <saiprasad78v@gmail.com>
2026-07-23 21:23:11 +0530

    problem of the day

---
+++ Basic to advance pro/dsa_with_java/BFSGraph.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class BFSGraph {
+    private int vertices;
+    private LinkedList<Integer>[] adjList;
+
+    BFSGraph(int v) {
+        vertices = v;
+        adjList = new LinkedList[v];
+
+        for (int i = 0; i < v; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int source, int destination) {
+        adjList[source].add(destination);
+        adjList[destination].add(source); // Undirected graph
+    }
+
+    void bfs(int start) {
+        boolean[] visited = new boolean[vertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        System.out.print("BFS Traversal: ");
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adjList[node]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BFSGraph graph = new BFSGraph(5);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 4);
+
+        graph.bfs(0);
+    }
+}
