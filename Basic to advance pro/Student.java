137484246ced7ee9a77a9c978fba057c0dd7c32c
saiprasad578 <saiprasad78v@gmail.com>
2026-07-25 19:30:48 +0530

    problem of the day

---
+++ Basic to advance pro/Student.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student {
+    int id;
+    String name;
+    double marks;
+
+    Student(int id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    void display() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+        System.out.println("----------------------");
+    }
+}
+
+public class StudentManagement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter details of Student " + (i + 1));
+
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Marks: ");
+            double marks = sc.nextDouble();
+
+            students.add(new Student(id, name, marks));
+        }
+
+        System.out.println("\nStudent Details:");
+        for (Student s : students) {
+            s.display();
+        }
+
+        sc.close();
+    }
+}
