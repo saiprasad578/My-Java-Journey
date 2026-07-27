71f5dd32675782f1a2025d7371d00c79c98914d5
saiprasad578 <saiprasad78v@gmail.com>
2026-07-27 22:59:03 +0530

    problem of the day

---
+++ Basic to advance pro/element.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class element {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int n = nums.length;
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (map.get(num) > n / 2) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
