aad1ef386e7b691db3e4e68c8e5154a60a55e844
sai prasad <saiprasad78v@gmail.com>
2026-08-20 21:16:49 +0530

    problem of the day

---
+++ Basic to advance pro/MergeSortedArrays.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+
+    public static int[] merge(int[] arr1, int[] arr2) {
+
+        int[] result = new int[arr1.length + arr2.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+
+            if (arr1[i] < arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < arr1.length) {
+            result[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while (j < arr2.length) {
+            result[k] = arr2[j];
+            j++;
+            k++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+
+        int[] result = merge(arr1, arr2);
+
+        System.out.println("Array 1: " + Arrays.toString(arr1));
+        System.out.println("Array 2: " + Arrays.toString(arr2));
+        System.out.println("Merged Array: " + Arrays.toString(result));
+    }
+}
