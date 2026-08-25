1af2967540015f86122bedebdd3b7dbc421ceaf4
sai prasad <saiprasad78v@gmail.com>
2026-08-25 21:07:42 +0530

    Problem of the Day

---
+++ Basic to advance pro/dsa_with_java/MaxSubarraySum.java
@@ -0,0 +1,28 @@
+public class MaxSubarraySum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        int windowSum = 0;
+
+        // Calculate first window
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        // Slide the window
+        for (int i = k; i < arr.length; i++) {
+
+            windowSum += arr[i];
+            windowSum -= arr[i - k];
+
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        System.out.println("Maximum Sum: " + maxSum);
+    }
+}
