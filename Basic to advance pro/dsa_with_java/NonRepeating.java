9be5bd36570256308a2c2656a98729d021237648
sai prasad <saiprasad78v@gmail.com>
2026-08-25 21:14:52 +0530

    Repeating terms are taken and check whether they are repeating or non repeating

---
+++ Basic to advance pro/dsa_with_java/NonRepeating.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class NonRepeating {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        boolean found = false;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                System.out.println("First non-repeating character: " + ch);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No non-repeating character found.");
+        }
+
+        sc.close();
+    }
+}
