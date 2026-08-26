263b97165ea77d87e540939e419ca456b812b3b8
saiprasad578 <saiprasad78v@gmail.com>
2026-08-26 16:44:37 +0530

    problem of the day

---
+++ Basic to advance pro/dsa_with_java/LinkedListCycle.java
@@ -0,0 +1,48 @@
+public class LinkedListCycle {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static boolean hasCycle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        // Create a cycle:
+        // 40 -> 20
+        head.next.next.next.next = head.next;
+
+        if (hasCycle(head)) {
+            System.out.println("Cycle detected in the linked list.");
+        } else {
+            System.out.println("No cycle found.");
+        }
+    }
+}
