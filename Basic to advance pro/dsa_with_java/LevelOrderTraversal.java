b01cd856d1d5a1d5f8dfb19f00d4fb666f56758e
sai prasad <saiprasad78v@gmail.com>
2026-08-27 21:46:41 +0530

    Create LevelOrderTraversal

---
+++ Basic to advance pro/dsa_with_java/LevelOrderTraversal.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class LevelOrderTraversal {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void levelOrder(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            Node current = queue.poll();
+
+            System.out.print(current.data + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.print("Level Order Traversal: ");
+
+        levelOrder(root);
+    }
+}
