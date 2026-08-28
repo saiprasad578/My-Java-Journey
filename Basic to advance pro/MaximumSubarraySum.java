b86a44bffa3233c2c3b0c128a76ef54334a15182
sai prasad <saiprasad78v@gmail.com>
2026-08-28 22:06:56 +0530

    Problem of the Day

---
+++ Basic to advance pro/MaximumSubarraySum.java
@@ -0,0 +1,26 @@
+public class MaximumSubarraySum {
+
+    static int maxSubarraySum(int[] arr) {
+
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int result = maxSubarraySum(arr);
+
+        System.out.println("Maximum Subarray Sum: " + result);
+    }
+}
